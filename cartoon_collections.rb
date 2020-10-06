15601e91a0a56d7c32c55798fef417ee5425d077
julio hernandez <julioandres72@gmail.com>
2020-10-06 21:33:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index do |array, index|
-    puts "#{index}#{array}"
+    puts "#{index}. #{array}"
   end
   # Use `each_with_index` to enumerate over the provided array
   #
